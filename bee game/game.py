e08a4bd7535d908e044569133aab0e408120aed7
FfionLee <120313877+FfionLee@users.noreply.github.com>
2024-08-29 16:59:12 +0100

    Update game.py

--- bee game/game.py
+++ bee game/game.py
@@ -12,11 +12,17 @@ bee.pos=200,200
 flower=Actor('flower')
 flower.pos=90,40
 
+g_o=False
+
 def draw():
     screen.blit('backgroundgrass',(0,0))
     bee.draw()
     flower.draw()
     screen.draw.text("Score: "+str(score),center=(40,20))
+
+    if g_o==True:
+        screen.fill('purple')
+        screen.draw.text('Your final score is'+str(score),center=(200,200))
 
 def update():
     global score
@@ -33,6 +39,11 @@ def update():
         flower.pos=random.randint(10,390),random.randint(10,390)
         score=score+1
 
+def gameover():
+    global g_o
+    g_o=True
 
+    print('jjjjjjjjjjjjjjjjjj')
 
+clock.schedule(gameover,5.0)
 pgzrun.go()
